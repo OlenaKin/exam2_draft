61ed69a3c7bbfc64a91883dc604e25402297af7c
Olena Onyshkiv <elena.onishkiv@gmail.com>
2024-08-26 17:16:55 +0200

    Added mediumZoom to the gallery

--- index.js
+++ index.js
@@ -1,76 +1,9 @@
 "use strict";
 
 
-/*
 
-let slideIndexes = { // Store slide indexes for both slide sets
-  mainSlides: 1,
-  forthSlides: 1
-};
-const time = 3000; // Time interval for automatic slide change
 
-// Function to show slides for a given set
-function showSlides(set, n) {
-  let i;
-  let slides;
-  let dots;
 
-  if (set === 'main') {
-    slides = document.getElementsByClassName("main__mySlides");
-    dots = document.getElementsByClassName("dot");
-  } else if (set === 'forth') {
-    slides = document.getElementsByClassName("forth-block__cardSlide");
-    dots = document.getElementsByClassName("forth-block_dot");
-  }
-
-  if (n > slides.length) { 
-    slideIndexes[`${set}Slides`] = 1; 
-  } 
-  if (n < 1) { 
-    slideIndexes[`${set}Slides`] = slides.length; 
-  }
-
-  // Hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-
-  // Remove the "active" class from all dots
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  // Show the current slide and mark the corresponding dot as active
-  slides[slideIndexes[`${set}Slides`] - 1].style.display = "block"; 
-  if (dots[slideIndexes[`${set}Slides`] - 1]) {
-    dots[slideIndexes[`${set}Slides`] - 1].className += " active";
-  }
-}
-
-// Function to change slides automatically for a given set
-function changeSlide(set) {
-  slideIndexes[`${set}Slides`]++;
-  showSlides(set, slideIndexes[`${set}Slides`]);
-}
-
-// Function to handle next/previous controls for a given set
-function plusSlides(set, n) {
-  showSlides(set, slideIndexes[`${set}Slides`] += n);
-}
-
-// Function to navigate to a specific slide for a given set
-function currentSlide(set, n) {
-  slideIndexes[`${set}Slides`] = n;
-  showSlides(set, slideIndexes[`${set}Slides`]);
-}
-
-// Start by showing the first slide for both sets
-showSlides('main', slideIndexes.mainSlides);
-showSlides('forth', slideIndexes.forthSlides);
-
-// Set the automatic slide change intervals for both sets
-setInterval(() => changeSlide('main'), time);
-setInterval(() => changeSlide('forth'), time);*/
 
 
 
@@ -196,7 +129,7 @@ windowElement.addEventListener("click", function (event) {
   }
 });*/
 
-const burgerMenu = document.getElementById("burgerMenu");
+/*const burgerMenu = document.getElementById("burgerMenu");
 const burgerButton = document.getElementById("burgerButton");
 const burgerToggle = document.getElementById("burgerToggle");
 
@@ -220,9 +153,13 @@ burgerMenu.addEventListener("click", function(event) {
     burgerToggle.checked = false; // Close menu if click outside menu items
     toggleBurgerMenu(); // Call the toggle function
   }
-});
+});*/
 
 
+
+mediumZoom('.zoom',{
+  margin:50
+})
 
 
 
